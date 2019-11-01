1106c620a6bde77411f7a0ba92bf8f702cabaa2b
KIM JUN SIK <jinaganda5@gmail.com>
2019-11-01 22:03:48 +0900

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+# Dictionary
+# key:value
+
+junsik = {
+    "name": "junsik",
+    "age": 25,
+    "isKorean": True,
+    "Favorite": ["Reading", "Alcohol"]
+}
+
+print(junsik)  # like obj of javascript
+
+# add something
+junsik["isSmart"] = True
+print(junsik)  # print junsick add more infomation
