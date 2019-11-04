84db8f259602aeab99bf34fc8699f14925b12a3c
KIM JUN SIK <jinaganda5@gmail.com>
2019-11-04 19:12:18 +0900

    if / elif / else

--- conditional.py
+++ conditional.py
@@ -7,3 +7,21 @@ def add(a, b):
 
 add(1, 2.1)
 add("1", 2.1)
+
+
+def age_check(age):
+    print(f"you are {age}.")
+    if age <= 12:
+        print("Never!")
+    elif age < 18 and age > 13:
+        print("you can't drink.")
+    elif age == 18 or age == 19:
+        print("you are new to this.")
+    else:
+        print("Have a drink.")
+
+
+age_check(12)
+age_check(15)
+age_check(19)
+age_check(22)
