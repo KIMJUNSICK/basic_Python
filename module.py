ebe894634f5848c6615fd5f86b780d30cf32eb0e
KIM JUN SIK <jinaganda5@gmail.com>
2019-11-04 19:33:17 +0900

    Module: from/import

---
+++ module.py
@@ -0,0 +1,8 @@
+import math  # whole module
+from math import ceil, floor, fabs as abs  # parts in module
+from calcurator import add
+
+print(abs(-1.2))
+print(ceil(1.2))
+print(floor(-1.2))
+add(1, 2)  # print 3
