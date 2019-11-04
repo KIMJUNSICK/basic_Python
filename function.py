9fa91086c2aed098105ab90fd0e24e44bae5a3e2
KIM JUN SIK <jinaganda5@gmail.com>
2019-11-04 11:31:01 +0900

    First Arguments & default value

--- function.py
+++ function.py
@@ -12,8 +12,25 @@ print(type(new_age))
 
 
 # Custom function
-def junsik():
-    print("junsikkim")
+def call_name(name="anonymous"):
+    print("Hello", name)
 
 
-junsik()
+call_name()
+call_name("junsik")
+
+
+def add(a, b):
+    print(a+b)
+
+
+add(1, 2)
+# add(1) =>  # arguments called 'b' is needed => add(a, b=0)
+
+
+def add2(a, b=0):
+    print(a+b)
+
+
+add2(1, 2)  # 3
+add2(1)  # 1
