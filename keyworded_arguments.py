5cbfb38a39d3e2b9393a7ff525ba6054eb751556
KIM JUN SIK <jinaganda5@gmail.com>
2019-11-04 11:50:07 +0900

    Keyworded arguments

---
+++ keyworded_arguments.py
@@ -0,0 +1,14 @@
+def minus(a, b):
+    print(a-b)
+
+
+minus(1, 2)  # -1
+minus(2, 1)  # 1
+
+
+def say_hello(name, age):
+    return f"Hello, {name}. your age is {age}"
+
+
+result = say_hello("junsik", 25)
+print(result)
